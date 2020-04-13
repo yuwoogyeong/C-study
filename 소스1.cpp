967e7f9cb965f8e0a831d2a228c09928a9e92a8b
yuwoogyeong <63202276+yuwoogyeong@users.noreply.github.com>
2020-04-13 17:24:59 +0900

    Add files via upload
    1주차 과제물 제출합니다.

---
+++ 소스1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int First,Second,one,two,three;
+	float four;
+
+	First = 3;
+	Second = 5;
+	cout << "First number>>" << First<<endl;
+	cout << "Second number>>" << Second<<endl;
+
+	one= First + Second ;
+	two = First - Second;
+	three = First * Second;
+	four = (double)First/ Second;  //First를 double로 명시적형변환을 해주면 3.0/5.0으로 바뀐다.
+	//그리고four가 float의 값을 가지니 따라서 값은 0.6이 나올것이다. 
+
+	cout << "=========계산 결과=========\n";
+	cout << First << "+" << Second << "=" << one << endl;
+	cout << First << "-" << Second << "=" << two << endl;
+	cout << First << "*" << Second << "=" << three << endl;
+	cout << First << "/" << Second << "=" << four << endl;
+}
